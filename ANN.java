177981146b50c7d0f820437a60d036c01f8f437a
Pranav Asthana (Partner) <pranav.i.asthana@happiestminds.com>
2018-04-05 15:04:15 +0530

    Done

--- ANN.java
+++ ANN.java
@@ -11,27 +11,33 @@ class ANN
     double[][] weights1 = new double[j][i+1];
     double[][] weights2 = new double[k][j+1];
 
+    // Momentum matrices for the 2 layers
     double[][] M1 = new double[j][i+1];
     double[][] M2 = new double[k][j+1];
 
+    // RMS matrices for the 2 layers
     double[][] R1 = new double[j][i+1];
     double[][] R2 = new double[k][j+1];
 
+    // Used for backpropagation
     ArrayList<Double> delta_j;
     ArrayList<Double> delta_k;
 
+    // Lists to store intermediate outputs
     ArrayList<Double> list_zi;
     ArrayList<Double> list_aj;
     ArrayList<Double> list_zj;
     ArrayList<Double> list_ak;
     ArrayList<Double> list_yk;
 
+    // Derivatives used for backpropagation
     double[][] derivatives_E_Wji = new double[j][i+1];
     double[][] derivatives_E_Wkj = new double[k][j+1];
 
-    boolean crossEntropy = true; // false -> sse
+    // Error fucntion used
+    boolean crossEntropy = true; // false -> Sum of Squared Errors
 
-    ANN(int hidden, boolean verbose)
+    ANN(int hidden, boolean verbose) // hidden = number of hidden neurons
     {
         j = hidden;
         this.verbose = verbose;
@@ -48,7 +54,8 @@ class ANN
         derivatives_E_Wji = new double[j][i+1];
         derivatives_E_Wkj = new double[k][j+1];
 
-        // Initialize weight matrices based on Gaussian distribution
+        // Initialize weight matrices based on Gaussian distribution with mean 0, variance 1
+        // Initialize momentum and rms matrices to 0
         Random r = new Random();
         for (int j = 0; j < this.j; j++)
         {
@@ -79,15 +86,17 @@ class ANN
         while(iterations++ < max_iterations)
         {
             error = 0;
-            ArrayList<ArrayList<Example>> batches = generateBatches(train_x, batch_size);
-            for (ArrayList<Example> batch: batches)
+            ArrayList<ArrayList<Example>> batches = generateBatches(train_x, batch_size); // Get batches
+            for (ArrayList<Example> batch: batches) // For each batch
             {
                 Matrix sum_derivatives_E_Wji = new Matrix(new double[j][i+1]);
                 Matrix sum_derivatives_E_Wkj = new Matrix(new double[k][j+1]);
-                for (Example xi: batch)
+                for (Example xi: batch) // For each sample
                 {
+                    // Get the outputs with the current weights
                     ArrayList<Double> y = this.feedForward(xi.attributes); // y.size() = k
 
+                    // Calculate error for the sample
                     double error_n = 0;
                     for (int k = 0; k < this.k; k++)
                     {
@@ -96,20 +105,22 @@ class ANN
                         else
                             error_n += (0.5)*Math.pow(y.get(k) - xi.target.get(k) ,2);
                     }
-                    error_n /= this.k;
-                    error += error_n;
+                    error_n /= this.k; // Normalize based on number of output classes
+                    error += error_n; // Add to total error
 
-                    delta_k = this.evaluateOutputDeltas(y, xi.target);
+                    delta_k = this.evaluateOutputDeltas(y, xi.target); // Calculate output delta values
 
-                    delta_j = this.backprop(this.list_aj, this.weights2, delta_k);
 
+                    delta_j = this.backprop(this.list_aj, this.weights2, delta_k); // Calculate hidden delta values using backpropagation
                     this.evaluateDerivatives(); // 2 matrices of derivatives
+                    // Sum derivatives for batch processing
                     sum_derivatives_E_Wji.add(new Matrix(derivatives_E_Wji));
                     sum_derivatives_E_Wkj.add(new Matrix(derivatives_E_Wkj));
                 }
+                // Perform gradient descent with the batch error derivatives
                 this.gradientDescent(weights1, weights2, M1, M2, sum_derivatives_E_Wji.matrix, sum_derivatives_E_Wkj.matrix, iterations);
             }
-            error /= train_x.size();
+            error /= train_x.size(); // Normalize total error based on number of samples
 
             double validation_error = getValidationError(validation_x);
             if (verbose)
@@ -119,7 +130,7 @@ class ANN
             }
             if ((validation_error - old_validation_error) > 0.01*old_validation_error)
                 spike++;
-            if (spike == 5) // Stop training when validation_error increases 5th time
+            if (spike == 5) // Stop training when validation_error increases for the 5th time
                 break;
             old_validation_error = validation_error;
         }
@@ -134,10 +145,10 @@ class ANN
     ArrayList<ArrayList<Example>> generateBatches(ArrayList<Example> train_x, int batch_size)
     {
         ArrayList<ArrayList<Example>> batches = new ArrayList<ArrayList<Example>>();
-        Collections.shuffle(train_x);
+        Collections.shuffle(train_x); // Randomly pick `batch_size` number of samples
         for (int i = 0; i < train_x.size(); i += batch_size)
         {
-            if (i+batch_size < train_x.size())
+            if (i+batch_size < train_x.size()) // Last batch may contain less samples
                 batches.add(new ArrayList(train_x.subList(i, i+batch_size)));
             else
                 batches.add(new ArrayList(train_x.subList(i, train_x.size())));
@@ -146,7 +157,7 @@ class ANN
     }
 
     // Perform gradient descent on weight matrix w by iterating over samples in x with learning rate eta
-    // Iterate until error < epsilon or number of iterations > max_iterations
+    // The adam optimizer has been implemented
     void gradientDescent(double[][] weights1, double[][] weights2, double[][] M1, double[][] M2, double[][] D_Wji, double[][] D_Wkj, int iteration)
     {
         // Hyper-parameters
@@ -165,7 +176,7 @@ class ANN
                 double R2_corrected = R2[k][j] / (1 - Math.pow(beta2, iteration));
                 weights2[k][j] -= eta * (M2_corrected / (Math.sqrt(R2_corrected) + epsilon));
 
-                // weights2[k][j] -= eta * D_Wkj[k][j];
+                // weights2[k][j] -= eta * D_Wkj[k][j]; // Simple gradient descent
             }
         }
 
@@ -179,7 +190,7 @@ class ANN
                 double R1_corrected = R1[j][i] / (1 - Math.pow(beta2, iteration));
                 weights1[j][i] -= eta * (M1_corrected / (Math.sqrt(R1_corrected) + epsilon));
 
-                // weights1[j][i] -= eta * D_Wji[j][i];
+                // weights1[j][i] -= eta * D_Wji[j][i]; // Simple gradient descent
             }
         }
 
@@ -208,7 +219,6 @@ class ANN
     {
         ArrayList<Double> delta_j = new ArrayList<Double>();
 
-        // Implementation here
         for (int j = 0; j <= this.j; j++)
         {
             double sum = 0;
@@ -217,7 +227,7 @@ class ANN
                 sum += weights_kj[k][j] * delta_k.get(k);
             }
             if (j == this.j)
-                delta_j.add(0.0d);
+                delta_j.add(0.0d); // For bias term
             else
                 delta_j.add(sum * delta_sigmoid(list_aj.get(j)));
         }
@@ -231,10 +241,7 @@ class ANN
         for (int k = 0; k < y.size(); k++)
         {
             if (crossEntropy)
-            {
-                // delta_k.add(-target.get(k) * (1-y.get(k))); // *delta_softmax = softmax*(1-softmax)
                 delta_k.add((y.get(k) - target.get(k)));
-            }
             else
                 delta_k.add((y.get(k) - target.get(k)) * delta_sigmoid(list_ak.get(k)));
         }
@@ -321,14 +328,12 @@ class ANN
     {
         Utility uObj = new Utility(); // Utility object
 
-        // int FP = 0, FN = 0, TP = 0, TN = 0; // Initialize variables
         int correct = 0, incorrect = 0;
         double test_error = 0;
 
         for (Example xi: test_x)
         {
             ArrayList<Double> y = this.feedForward(xi.attributes);
-            ArrayList<Integer> prediction = new ArrayList<Integer>();
             double En = 0;
             int p = -1;
             double max_prob = 0;
@@ -377,6 +382,14 @@ class ANN
         return 1 / (1 + Math.exp(-x));
     }
 
+    static void showHelp()
+    {
+        System.out.println("[USAGE]: java Regression TRAIN_DATA TEST_DATA VALIDATION_DATA [options]");
+        System.out.println("Options:");
+        System.out.println("-v --verbose: Verbose output");
+        System.out.println("-h --help: Show this help message");
+    }
+
     public static void main(String[] args)
     {
         Utility uObj = new Utility(); // Utility object
@@ -398,12 +411,12 @@ class ANN
                     verbose = true;
                 if (options.equals("-h") || options.equals("--help"))
                 {
-                    // showHelp(); TODO: Add help
+                    showHelp();
                     System.exit(0);
                 }
             }catch(Exception e){}
         } catch(Exception e) {
-            // showHelp(); TODO: Add help
+            showHelp();
             System.exit(0);
         }
 
